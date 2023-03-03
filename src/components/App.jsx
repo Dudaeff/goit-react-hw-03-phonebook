21736f89907a55342fb6eb609780177564f8486b
Roma <r.dutkevich@gmail.com>
2023-03-03 09:40:32 +0200

    added local storage services

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { nanoid } from "nanoid";
 import { Component } from "react"
+import { getFromLocal, saveToLocal } from "services/Storage/Storage";
 import { ContactForm } from "./ContactForm/ContactForm";
 import { ContactList } from "./ContactList/ContactList";
 import { Filter } from "./Filter/Filter";
@@ -7,14 +8,21 @@ import { Section } from "./Section/Section";
 
 export class App extends Component  {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    this.setState({
+      contacts: getFromLocal('contacts')
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      saveToLocal('contacts', this.state.contacts)
+    }
+  }
   
   onContactFormSubmit = (evt) => {
     evt.preventDefault();
